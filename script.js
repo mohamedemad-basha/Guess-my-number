229382cf9288bfa84dad1f8fd3c342b6d16db006
mohamedemad-basha <144592551+mohamedemad-basha@users.noreply.github.com>
2025-09-13 14:43:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+let randomValue = Math.trunc(Math.random() * 20) + 1;
+let score = 20,
+  highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) displayMessage('⛔ No number!');
+  else if (guess === randomValue) {
+    document.querySelector('.number').textContent = randomValue;
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.background = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== randomValue) {
+    if (score > 1) {
+      displayMessage(guess > randomValue ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  randomValue = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = 'Start guessing...';
+});
